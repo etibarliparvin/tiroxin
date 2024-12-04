75d48a1be31076bfe3b3a6917e3842967258fa31
Timofey Khutoryanskiy <tiroxxxin@gmail.com>
2024-12-04 15:51:32 +0500

    tiroxn/B-1 first commit

--- src/main/java/com/faang/Main.java
+++ src/main/java/com/faang/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hi from Parvin");
         System.out.println("I hope you are well Tiroxin...");
+        System.out.println("i am well");
     }
 }
