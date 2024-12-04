f8a6a96af993a9910b8e9af90b83530d0f14ea99
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-04 15:35:21 +0400

    etibarliparvin-A-6 erased everything and added my new feature

--- src/main/java/com/faang/Main.java
+++ src/main/java/com/faang/Main.java
@@ -2,16 +2,6 @@ package com.faang;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hi from Parvin");
-        System.out.println("I hope you are well Tiroxin...");
-        System.out.println("i am well");
-        System.out.println("i have added this line while you are working on your branch");
-        System.out.println("feature 1");
-        System.out.println("feature 2");
-        System.out.println("feature 3");
-        System.out.println("feature 4");
-        System.out.println("feature 5");
-        System.out.println("new feature from A-5");
+        System.out.println("A-6");
     }
 }
