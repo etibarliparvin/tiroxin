c025e22b6e9c16d95b1be7f8648481f4a172d43e
Parvin Etibarli <etibarliparvin@gmail.com>
2024-12-04 15:29:04 +0400

    etibarliparvin-A-4 new feature

--- src/main/java/com/faang/Main.java
+++ src/main/java/com/faang/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("feature 2");
         System.out.println("feature 3");
         System.out.println("feature 4");
+        System.out.println("feature 5");
     }
 }
